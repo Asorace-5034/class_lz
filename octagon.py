804d29bc0d35e3793f77e4c930e5cbe9c20ff4df
Asorace-5034 <tafytoo@gmail.com>
2026-03-02 22:50:29 +0300

    Add files via upload

--- octagon.py
+++ octagon.py
@@ -1,4 +1,3 @@
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -14,7 +13,7 @@ class Octago:
         return area, perimetor
     
     def opis_octa(self):
-        radius_op = self.length / (2 * 3.14 / 8)
+        radius_op = self.length / (2 - 2 ** 0.5) ** 0.5
         area = 3.14 * radius_op ** 2
         return radius_op, area
 
@@ -33,14 +32,13 @@ class Octago:
         x = [r_out * np.cos(i * np.pi / 4) for i in range(8)]
         y = [r_out * np.sin(i * np.pi / 4) for i in range(8)]
         
-        ax.fill(x, y, color='black', label='Октогон')
+        ax.fill(x, y, color='black')
 
-        circle_out = plt.Circle((0, 0), r_out, color="blue", fill=False, label="Описанная окружность")
+        circle_out = plt.Circle((0, 0), r_out, color="blue", fill=False)
         ax.add_patch(circle_out)
 
-        circle_in = plt.Circle((0, 0), r_in, color="red", fill=False, label="Вписанная окружность")
+        circle_in = plt.Circle((0, 0), r_in, color="red", fill=False)
         ax.add_patch(circle_in)
 
-        plt.legend()
         plt.grid()
         plt.show()
